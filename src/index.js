56af9c4e6cabe8b709a70f4f35f8880021823eac
devtescode <teslimagboola09@gmail.com>
2026-06-26 22:32:38 +0100

    update code

--- src/index.js
+++ src/index.js
@@ -13,25 +13,17 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 import { applyTheme, getInitialTheme } from "./hooks/useTheme";
 import App from "./App";
-import { register } from "./serviceWorkerRegistration";
+// import { register } from "./serviceWorkerRegistration";
 
 applyTheme(getInitialTheme());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 root.render(
   <React.StrictMode>
-    <h1 style={{ color: "white", padding: "20px" }}>
-      Hello Lumina AI
-    </h1>
     <App />
   </React.StrictMode>
 );
 
 // Register the service worker
-register();
+// register();
