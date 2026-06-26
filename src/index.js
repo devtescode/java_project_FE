27127555675feb9f75953fa85dec25f617e29312
devtescode <teslimagboola09@gmail.com>
2026-06-26 21:48:24 +0100

    update code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global.css';
-import { applyTheme, getInitialTheme } from './hooks/useTheme';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './styles/global.css';
+// import { applyTheme, getInitialTheme } from './hooks/useTheme';
+// import App from './App';
+
+// applyTheme(getInitialTheme());
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<React.StrictMode><App /></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+import { applyTheme, getInitialTheme } from "./hooks/useTheme";
+import App from "./App";
+import { register } from "./serviceWorkerRegistration";
 
 applyTheme(getInitialTheme());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Register the service worker
+register();
